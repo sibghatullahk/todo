81327de2b1585543f48c44f45b30b9fd31e9697b
Sibghat ullah <sibghatullahk83@gmail.com>
2024-04-07 16:01:00 +0500

    updated to do

---
+++ index.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+//Creating a todo list using inquirer
+//push, pop, concat
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What do you want to add to your todo list?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add another task?",
+            default: false
+        },
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
